ddd425d01a1a40d235df792906397361b676b277
josepLlMt20 <101903899+josepLlMt20@users.noreply.github.com>
2023-10-30 13:10:38 +0100

    Va el main

--- src/Controller.java
+++ src/Controller.java
@@ -1,8 +1,6 @@
-import java.lang.reflect.Array;
-import java.lang.reflect.InaccessibleObjectException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 
 public class Controller {
@@ -11,19 +9,24 @@ public class Controller {
 
     public Controller() {
         listInvokers = new ArrayList<Invoker>();
-        for(int i = 0;i<10;i++){
+        for (int i = 0; i < 10; i++) {
             this.listInvokers.add(i, new Invoker());
         }
         this.mapActions = new HashMap<>();
     }
 
-     public void registerAction (String action, Function f) {
-         this.mapActions.put(action, f);
-     }
+    public void registerAction(String action, Function<Map<String, Integer>, Integer> f) {
+        this.mapActions.put(action, f);
+    }
 
-     public void invoke (String action, Object o){
+    public Object invoke(String actionName, Map<String, Integer> params) {
+        Function<Map<String, Integer>, Integer> action = mapActions.get(actionName);
+        if (action != null) {
+            return action.apply(params);
+        } else {
+            throw new IllegalArgumentException("Action not found: " + actionName);
+        }
 
-     }
 
-
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
+import java.util.Map;
+import java.util.function.Function;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         Controller controller = new Controller();
+
+        Function<Map<String, Integer>, Integer> f = x -> x.get("x") - x.get("y");
+        controller.registerAction("addAction", f);
+        int res = (int) controller.invoke("addAction", Map.of("x", 6, "y", 2));
+        System.out.println(res);
+
 
     }
 }
