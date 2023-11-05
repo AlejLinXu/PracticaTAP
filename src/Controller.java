8d3957ac1442af4848bc8f9149410ed78664e57e
AlejLinXu <zhenghaoalejandro.lin@estudiants.urv.cat>
2023-11-05 14:50:54 +0100

    Main con Lista funciona

--- src/Controller.java
+++ src/Controller.java
@@ -1,15 +1,14 @@
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 
 public class Controller<T, V> {
-    private ArrayList<Invoker> listInvokers;
-    private HashMap<String, Function<T, V>> mapActions;
+    private final ArrayList<Invoker> listInvokers;
+    private final HashMap<String, Function<T, V>> mapActions;
 
     public Controller() {
-        listInvokers = new ArrayList<Invoker>();
+        listInvokers = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             this.listInvokers.add(i, new Invoker());
         }
@@ -22,15 +21,9 @@ public class Controller<T, V> {
 
     public Object invoke(String actionName, Map<String, Integer> params) {
         Function<Object, Object> action = (Function<Object, Object>) mapActions.get(actionName);
+        if (action != null){
             return listInvokers.get(0).executeAction(action, params);
-    }
-
-    //getter and setters
-    public ArrayList<Invoker> getListInvokers() {
-        return listInvokers;
-    }
-
-    public HashMap<String, Function<T, V>> getMapActions() {
-        return mapActions;
+        }
+        else throw new IllegalArgumentException("Action not registered: " + actionName);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,23 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        Controller controller = new Controller();
+        Controller<Map<String, Integer>, Integer> controller = new Controller<>();
         Function<Map<String, Integer>, Integer> f = x -> x.get("x") - x.get("y");
         controller.registerAction("addAction", f);
         int res = (int) controller.invoke("addAction", Map.of("x", 6, "y", 2));
         System.out.println(res);
 
-        /*
-        List<Map<String, Integer>> input = Arrays.asList(new Map[]{
+
+        List<Map<String, Integer>> input = Arrays.asList(
                 Map.of("x", 2, "y", 3),
                 Map.of("x", 9, "y", 1),
-                Map.of("x", 8, "y", 8),
+                Map.of("x", 8, "y", 8)
+        );
+        input.forEach(inputs -> {
+            int itemList = (int) controller.invoke("addAction", inputs);
+            System.out.println(itemList);
         });
-        List<Integer> result = (List<Integer>) controller.invoke("addAction",  input);
-        System.out.println(result.toString());
-        */
     }
 }
